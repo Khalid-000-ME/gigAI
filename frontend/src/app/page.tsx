5e33e184b0525fe904c6e4b708795fa8037616d5
Khalid-000-ME <imtheuser3@gmail.com>
2025-01-17 00:14:35 +0530

    Added effects to first page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,8 @@
 import Image from "next/image";
 import { ModeToggle } from "@/components/ui/mode-toggle";
+import './globals.css'
+import { Meteors } from "@/components/ui/meteors";
+
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-start h-screen">
@@ -7,9 +10,10 @@ export default function Home() {
         <div className="flex items-center m-4 justify-center"><ModeToggle /></div>
       </header>
       <main className="flex flex-col items-center justify-center w-full">
-        <div className="flex flex-col justify-center items-center m-4 w-[90vw] h-screen rounded-lg p-4">
-          <h1 className="text-4xl font-bold">Hello World</h1>
-          <p className="text-lg">This is a test</p>
+        <div className="flex flex-col justify-center items-center m-4 w-[90vw] h-[80vh] rounded-lg p-4 relative overflow-hidden">
+          <h1 className="z-10 text-4xl font-bold">Hello World</h1>
+          <p className="z-10 text-lg">This is a test</p>
+          <Meteors number={20} />
         </div>
       </main>
     </div>
